d00ad4cf1c5e12035ad71d742c7d117808a610a7
xyzell <alif.billah122014@gmail.com>
2024-11-29 04:21:48 +0700

    Penambahan slicing splash screen dan animasi

--- app/src/main/java/com/dicoding/matchsense/MainActivity.kt
+++ app/src/main/java/com/dicoding/matchsense/MainActivity.kt
@@ -2,8 +2,6 @@ package com.dicoding.matchsense
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
