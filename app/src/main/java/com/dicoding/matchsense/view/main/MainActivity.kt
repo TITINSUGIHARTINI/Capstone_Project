bf3dba1aaf716da2546777a1202a3c6865d212b4
XZenma <ferorefadhazaidan@gmail.com>
2024-12-05 02:23:10 +0700

    Splash Screen update

--- app/src/main/java/com/dicoding/matchsense/view/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/matchsense/view/main/MainActivity.kt
@@ -4,12 +4,16 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dicoding.matchsense.R
+import com.dicoding.matchsense.view.signup.SignupActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +25,49 @@ class MainActivity : AppCompatActivity() {
             setKeepOnScreenCondition {
                 !mainViewModel.isReady.value
             }
+            setOnExitAnimationListener { screen ->
+                val zoomX = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.4f,
+                    0.0f
+                )
+                zoomX.duration = 500L
+
+                val zoomY = ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.4f,
+                    0.0f
+                )
+                zoomY.duration = 500L
+
+                val fadeOut = ObjectAnimator.ofFloat(
+                    screen.view,
+                    View.ALPHA,
+                    1f,
+                    0f
+                )
+                fadeOut.duration = 300L
+                fadeOut.doOnEnd {
+                    screen.remove()
+                    isLogin()
+                }
+
+                zoomX.start()
+                zoomY.start()
+                fadeOut.start()
+            }
         }
-        setContentView(R.layout.activity_main)
+    }
+
+    private fun isLogin() {
+        if(!user.IsLogin) {
+            val intent = Intent(this@MainActivity, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            setContentView(R.layout.activity_main)
+        }
     }
 }
