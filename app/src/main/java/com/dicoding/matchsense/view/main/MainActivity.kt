10283ae87901576806c73e836fe2ae7d174975df
XZenma <ferorefadhazaidan@gmail.com>
2024-12-05 16:53:50 +0700

    Splash Screen update

--- app/src/main/java/com/dicoding/matchsense/view/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/matchsense/view/main/MainActivity.kt
@@ -1,28 +1,18 @@
 package com.dicoding.matchsense.view.main
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import com.dicoding.matchsense.R
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
-import android.animation.PropertyValuesHolder
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.view.animation.OvershootInterpolator
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dicoding.matchsense.R
-import com.dicoding.matchsense.view.signup.SignupActivity
 import com.dicoding.matchsense.databinding.ActivityMainBinding
 import com.dicoding.matchsense.view.ViewModelFactory
 import com.dicoding.matchsense.view.welcome.WelcomeActivity
@@ -37,56 +27,63 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        
+
+        val themeName = packageManager.getActivityInfo(componentName, PackageManager.GET_META_DATA)
+            .theme.toString()
+
         installSplashScreen().apply {
             setKeepOnScreenCondition {
                 !mainViewModel.isReady.value
             }
-            setOnExitAnimationListener { screen ->
-                val zoomX = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_X,
-                    0.4f,
-                    0.0f
-                )
-                zoomX.duration = 500L
-
-                val zoomY = ObjectAnimator.ofFloat(
-                    screen.iconView,
-                    View.SCALE_Y,
-                    0.4f,
-                    0.0f
-                )
-                zoomY.duration = 500L
-
-                val fadeOut = ObjectAnimator.ofFloat(
-                    screen.view,
-                    View.ALPHA,
-                    1f,
-                    0f
-                )
-                fadeOut.duration = 300L
-                fadeOut.doOnEnd {
-                    screen.remove()
-                    isLogin()
-                }
-
-                zoomX.start()
-                zoomY.start()
-                fadeOut.start()
-            }
+//            setOnExitAnimationListener { screen ->
+//                val zoomX = ObjectAnimator.ofFloat(
+//                    screen.iconView,
+//                    View.SCALE_X,
+//                    0.4f,
+//                    0.0f
+//                )
+//                zoomX.duration = 500L
+//
+//                val zoomY = ObjectAnimator.ofFloat(
+//                    screen.iconView,
+//                    View.SCALE_Y,
+//                    0.4f,
+//                    0.0f
+//                )
+//                zoomY.duration = 500L
+//
+//                val fadeOut = ObjectAnimator.ofFloat(
+//                    screen.view,
+//                    View.ALPHA,
+//                    1f,
+//                    0f
+//                )
+//                fadeOut.duration = 300L
+//                fadeOut.doOnEnd {
+//                    screen.remove()
+//                    isLogin()
+//                }
+//
+//                zoomX.start()
+//                zoomY.start()
+//                fadeOut.start()
+//            }
         }
+        isLogin()
     }
-    
+
     private fun isLogin() {
-        if(!user.IsLogin) {
-            val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        } else {
-            setContentView(binding.root)
-            setSupportActionBar(binding.toolbar)
+        mainViewModel.getSession().observe(this) { user ->
+            Log.d("MainActivity", "User: $user")
+            if (!user.isLogin) {
+                val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+                setSupportActionBar(binding.toolbar)
+            }
         }
     }
 
--- app/src/main/java/com/dicoding/matchsense/view/main/MainViewModel.kt
+++ app/src/main/java/com/dicoding/matchsense/view/main/MainViewModel.kt
@@ -1,6 +1,7 @@
 package com.dicoding.matchsense.view.main
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
@@ -9,15 +10,19 @@ import com.dicoding.matchsense.data.repository.UserRepository
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: UserRepository,) : ViewModel() {
     private val _isReady = MutableStateFlow(false)
     val isReady = _isReady.asStateFlow()
 
+    private val _isLoggedIn = MutableLiveData(false)
+    val isLoggedIn : LiveData<Boolean> = _isLoggedIn
+
     init {
         viewModelScope.launch {
-            delay(3000)
+            _isLoggedIn.value  = getSession().value?.isLogin
             _isReady.value = true
         }
     }
